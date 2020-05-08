b9d221183a2485da109db43a92cf776b3b126021
starfleetorg <43088142+starfleetorg@users.noreply.github.com>
2020-05-08 00:35:41 -0700

    Update ethernet.sh

--- kernel-configs/ethernet.sh
+++ kernel-configs/ethernet.sh
@@ -4,6 +4,6 @@ echo "dwc2" | sudo tee -a /etc/modules
 echo "libcomposite" | sudo tee -a /etc/modules
 
 # Enable the script to run on startup
-sudo echo 'bash /opt/starfleet/programs/Ethernet/main.sh' >> /etc/rc.local
-sudo echo 'bash /opt/starfleet/programs/Ethernet/main.sh' >> /etc/rc.local
+sudo echo 'bash /opt/starfleetorg/programs/Ethernet/main.sh' >> /etc/rc.local
+sudo echo 'bash /opt/starfleetorg/programs/Ethernet/main.sh' >> /etc/rc.local
 
